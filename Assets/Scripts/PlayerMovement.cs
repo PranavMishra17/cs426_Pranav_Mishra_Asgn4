9e5f834bd4a3ae38b36f9666ac8bdd471216fafe
jmielc2 <jacob.mielczarek@gmail.com>
2024-02-01 19:15:13 -0600

    Added player jump and improved object control
    I won't implement mouse movement since it makes the camera harder to control especially when switching windows

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class PlayerMovement : NetworkBehaviour {
-    public float speed = 2f;
-    public float rotationSpeed = 70f;
+public class PlayerMovement : NetworkBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 5f;
+    public float jumpForce = 300f;
     public List<Color> colors = new List<Color>();
-    public GameObject hand;
 
+    [SerializeField] private GameObject hand;
     [SerializeField] private AudioListener audioListener;
     [SerializeField] private Camera playerCamera;
     private Package dataPackage;
@@ -17,64 +19,88 @@ public class PlayerMovement : NetworkBehaviour {
         this.dataPackage = null;
     }
 
-    void Update() {        
+    void Update()
+    {        
         if (this.IsOwner) { // makes sure the script is only executed on the owners
             Vector3 moveDirection = new Vector3(0, 0, 0);
             Quaternion rotation = Quaternion.identity;
-            if (Input.GetKey(KeyCode.W)) {
+
+            // Player Position
+            if (Input.GetKey(KeyCode.W))
+            {
                 moveDirection = this.transform.forward;
             }
-            if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.S))
+            {
                 moveDirection = this.transform.forward * -1f;
             }
-            if (Input.GetKey(KeyCode.A)) {
+
+            // Player Rotation
+            if (Input.GetKey(KeyCode.A))
+            {
                 rotation = Quaternion.Euler(0f, -1f * rotationSpeed * Time.deltaTime, 0f);
             }
-            if (Input.GetKey(KeyCode.D)) {
+            if (Input.GetKey(KeyCode.D))
+            {
                 rotation = Quaternion.Euler(0f, rotationSpeed * Time.deltaTime, 0f);
             }
             transform.position += moveDirection * speed * Time.deltaTime;
             transform.rotation *= rotation;
 
+            // Object Interaction
             if (Input.GetKeyDown(KeyCode.E))
             {
                 AttemptInteractServerRpc();
+                if (!this.IsServer)
+                {
+                    this.AttemptInteract();
+                }
+            }
+            
+            // Jumping
+            if (Input.GetButtonDown("Jump"))
+            {
+                this.transform.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * this.jumpForce);
             }
         }
     }
 
-    [ServerRpc]
-    private void AttemptInteractServerRpc() {
+    private void AttemptInteract()
+    {
         if (this.dataPackage) {
             this.dropPackage();
-        } else {
+        } else
+        {
             RaycastHit hit;
             if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 2f, LayerMask.GetMask("Interactables")))
             {
                 this.dataPackage = hit.transform.gameObject.GetComponent<Package>();
                 if (this.dataPackage.hasHolder())
                 {
+                    this.dataPackage = null;
                     return;
                 }
-                Debug.Log("Picked up package.");
                 this.dataPackage.setHolder(this.hand);
-            } else
-            {
-                Debug.Log("Miss");
             }
         }
     }
 
+    [ServerRpc]
+    private void AttemptInteractServerRpc()
+    {
+        this.AttemptInteract();
+    }
 
-    public void dropPackage() {
-        Debug.Log("Dropping package.");
+
+    public void dropPackage()
+    {
         this.dataPackage.drop();
         this.dataPackage = null;
     }
 
     // this method is called when the object is spawned
-    public override void OnNetworkSpawn() {
-        Debug.Log("Spawned player.");
+    public override void OnNetworkSpawn()
+    {
         // Set object color
         GetComponent<MeshRenderer>().material.color = colors[(int)this.OwnerClientId % colors.Count];
 
@@ -90,6 +116,15 @@ public class PlayerMovement : NetworkBehaviour {
             playerCamera.gameObject.SetActive(true);
             audioListener.enabled = true;
             playerCamera.enabled = true;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (this.IsServer) {
+            if (this.dataPackage != null) {
+                this.dropPackage();
+            }
         }
     }
 }

6af67bcb9ba5a4a8896176e11c80159a6d2b0751
Pranav Mishra <89926012+PranavMishra17@users.noreply.github.com>
2024-02-04 14:29:19 -0600

    Added working battery surge with third person

--- Assets/Scripts/TPC.cs
+++ Assets/Scripts/TPC.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEditor.SceneView;
+using Unity.Netcode;
 
-public class TPC : MonoBehaviour
+public class TPC : NetworkBehaviour
 {
     public float speed = 7.5f;
     public float jumpSpeed = 8.0f;
@@ -17,49 +18,67 @@ public class TPC : MonoBehaviour
     Vector3 moveDirection = Vector3.zero;
     Vector2 rotation = Vector2.zero;
 
+    [SerializeField] private GameObject hand;
+
     [HideInInspector]
     public bool canMove = true;
+    private Package dataPackage;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         rotation.y = transform.eulerAngles.y;
+        this.dataPackage = null;
     }
 
     void Update()
     {
-        if (characterController.isGrounded)
+        if (this.IsOwner)
         {
-            // We are grounded, so recalculate move direction based on axes
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
-            float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
-            float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
-            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-            if (Input.GetButton("Jump") && canMove)
+            if (characterController.isGrounded)
             {
-                moveDirection.y = jumpSpeed;
+                // We are grounded, so recalculate move direction based on axes
+                Vector3 forward = transform.TransformDirection(Vector3.forward);
+                Vector3 right = transform.TransformDirection(Vector3.right);
+                float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
+                float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+                moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+                if (Input.GetButton("Jump") && canMove)
+                {
+                    moveDirection.y = jumpSpeed;
+                }
+            }
+
+            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+            // as an acceleration (ms^-2)
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            // Move the controller
+            characterController.Move(moveDirection * Time.deltaTime);
+
+            // Player and Camera rotation
+            if (canMove)
+            {
+                rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+                rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+                rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+                playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+                transform.eulerAngles = new Vector2(0, rotation.y);
+            }
+
+            // Object Interaction
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                AttemptInteractServerRpc();
+                if (!this.IsServer)
+                {
+                    this.AttemptInteract();
+                }
             }
         }
-
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
-
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
-
-        // Player and Camera rotation
-        if (canMove)
-        {
-            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-            playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
-            transform.eulerAngles = new Vector2(0, rotation.y);
-        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -69,6 +88,40 @@ public class TPC : MonoBehaviour
         {
             SpawnBloodSplatter();
         }
+    }
+
+    private void AttemptInteract()
+    {
+        if (this.dataPackage)
+        {
+            this.dropPackage();
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, 2f, LayerMask.GetMask("Interactables")))
+            {
+                this.dataPackage = hit.transform.gameObject.GetComponent<Package>();
+                if (this.dataPackage.hasHolder())
+                {
+                    this.dataPackage = null;
+                    return;
+                }
+                this.dataPackage.setHolder(this.hand);
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void AttemptInteractServerRpc()
+    {
+        this.AttemptInteract();
+    }
+
+    public void dropPackage()
+    {
+        this.dataPackage.drop();
+        this.dataPackage = null;
     }
 
 
